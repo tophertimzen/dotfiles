2041df4f753ab2802baff20b28afb5568ef3cc02
tophertimzen <c.timzen@gmail.com>
2016-02-14 11:15:53 -0800

    My dotfiles

---
+++ .zshrc
@@ -0,0 +1,31 @@
+export ZSH=$HOME/.oh-my-zsh
+
+ZSH_THEME="topher"
+
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+
+source $ZSH/oh-my-zsh.sh
+source $HOME/.alias
+source $HOME/.exploits
+
+SAVEHIST=1000
+HISTFILE=$HOME/.zsh_history
+
+# Base styles and color palette
+# Solarized colors
+# https://github.com/altercation/solarized/tree/master/iterm2-colors-solarized
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+SOLAR_YELLOW=$(tput setaf 136)
+SOLAR_ORANGE=$(tput setaf 166)
+SOLAR_RED=$(tput setaf 124)
+SOLAR_MAGENTA=$(tput setaf 125)
+SOLAR_VIOLET=$(tput setaf 61)
+SOLAR_BLUE=$(tput setaf 33)
+SOLAR_CYAN=$(tput setaf 37)
+SOLAR_GREEN=$(tput setaf 64)
+SOLAR_WHITE=$(tput setaf 254)
+
+gg () {
+   grep $1 * -sirn
+}
---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#Install oh-my-zsh if not already
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#Install Topher's ZSH theme
+cp topher.zsh-theme ~/.oh-my-zsh/themes/
+
+#copy and backup dot files
+if [ ! -d ~/.dotfileBackups/$(date "+%F") ]; then
+    echo "[+] Backing up dotfiles"
+    mkdir -p ~/.dotfileBackups/$(date "+%F")/
+    mkdir -p ~/.vim
+    mv ~/.alias ~/.dotfileBackups/$(date "+%F")/
+    mv ~/.vimrc ~/.dotfileBackups/$(date "+%F")/
+    mv ~/.vim ~/.dotfileBackups/$(date "+%F")/
+    mv ~/.zshrc ~/.dotfileBackups/$(date "+%F")/
+    echo "[+] Installing new dotfiles"
+    cp .alias ~/.alias
+    cp vim/.vimrc ~/.vimrc
+    cp -r vim/.vim ~/.vim
+    cp .zshrc ~/.zshrc
+    cp .exploits ~/.exploits
+    cp .gitconfig ~/.gitconfig
+fi
+
+
+#check if babun 
+if [ -d /cygdrive/ ]; then
+    echo "[+] Cygwin/Babun detected"
+    echo "[+] Installing mintty and cygwinAlias"
+    echo 'source $HOME/.cygwinAlias' >> ~/.zshrc
+    mv ~/.minttyrc ~/.dotfileBackups/$(date "+%F")/
+    cp .minttyrc ~/.minttyrc
+    cp .cygwinAlias ~/.cygwinAlias
+else 
+    echo "[+] Installing dark solarized theme for GNOME"
+    git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
+    cd gnome-terminal-colors-solarized
+    ./set_dark.sh
+    cd ..
+fi
+
