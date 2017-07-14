1b78e086a049d5e71c4ba514d751c1271df655df
tophertimzen <c.timzen@gmail.com>
2017-07-13 20:14:30 -0700

    Just have install.sh do all the work

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+# download Vundle files
+if [ ! -d ~/.vim/bundle ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
+fi
+
+if [ ! -d ~/.oh-my-zsh ];then
+  git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
+
+cp zsh/themes/topher.zsh-theme ~/.oh-my-zsh/themes/
+
 for dotfile in $(ls); do stow $dotfile; done
 
 echo "[+] Installing YouCompleteMe and vim plugins. This will might take a while."
